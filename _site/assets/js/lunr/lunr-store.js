4cddcbd49ffda70b2635f4c58945ec268402d6d7
goheeji <gmlwl0489@daum.net>
2020-11-14 02:52:08 +0900

    WIP

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Firstpost",
+        "excerpt":"test  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/firstpost/",
+        "teaser": null
+      }]
