55ee4d51201562df66f5c48e1eaabbfd1d4914d2
goheeji <gmlwl0489@daum.net>
2020-11-16 22:34:26 +0900

    하이라이트 색 변경

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Firstpost",
-        "excerpt":"test  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/firstpost/",
+        "title": "Notice ",
+        "excerpt":"**Notice.:** 기본적인 Notice {: .notice} **Promary Notice** 중요한 Notice {: .notice--primary} **Info Notice** 정보 Notice {: .notice--info} **warning Notice:** 경고 Notice {: .notice--warning} **Danger Notice:** Danger Notice {: .notice--danger} **Success Notice:** Success Notice {: .notice--success} 적용예: Notice.: 기본적인 Notice Promary Notice 중요한 Notice Info Notice 정보 Notice warning Notice: 경고...","categories": ["빵부스러기"],
+        "tags": ["notice","jekyll"],
+        "url": "http://localhost:4000/%EB%B9%B5%EB%B6%80%EC%8A%A4%EB%9F%AC%EA%B8%B0/Notice/",
+        "teaser": null
+      },{
+        "title": "마크다운(markdown) 문법 총정리",
+        "excerpt":"1. 헤더 Headers 큰제목: 문서제목 This is a H1 ============= 적용예: This is a H1 작은제목: 문서 부제목 This is a H2 ---------------- 적용예: This is an H2 글머리: 1~6까지만 지원 # This is a H1 ## This is a H2 ### This is a H3 #### This is a...","categories": ["Markdown"],
+        "tags": ["Markdown"],
+        "url": "http://localhost:4000/markdown/markdown-Syntax/",
         "teaser": null
       }]
